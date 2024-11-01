53df34e90ad6b2fe44c0f0128ced6ed3f3723700
mark2013 <mark@kreine.com>
2024-11-01 20:24:39 +0100

    Weather application in React JS

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./index.css";
+import Weather from "./components/Weather";
+
+export const App = () => {
+	return (
+		<div className="app">
+			<Weather />
+		</div>
+	);
+};
